244b3bc618b46543f7d9194329c29e138fbcb3b9
rahaf-af <rahaf77553@gmail.com>
2026-07-12 08:29:57 +0300

    refactor(user-reg): split name input into first name and last name

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -7,7 +7,8 @@ import { message } from "antd";
 
 
 const registerDataStandards = z.object({
-    name: z.string({ error: (issue) => (issue.input === undefined ? 'NAME_REQUIRED' : undefined) }).trim().min(2, 'NAME_TOO_SHORT').max(50, 'NAME_TOO_LONG').regex(/^[\u0600-\u06FFa-zA-Z\s]+$/, 'NAME_INVALID_CHARACTERS'),
+    firstName: z.string({ error: (issue) => (issue.input === undefined ? 'FIRST_NAME_REQUIRED' : undefined) }).trim().min(2, 'FIRST_NAME_TOO_SHORT').max(50, 'FIRST_NAME_TOO_LONG').regex(/^[\u0600-\u06FFa-zA-Z\s]+$/, 'FIRST_NAME_INVALID_CHARACTERS'),
+    lastName: z.string({ error: (issue) => (issue.input === undefined ? 'LAST_NAME_REQUIRED' : undefined) }).trim().min(2, 'LAST_NAME_TOO_SHORT').max(50, 'LAST_NAME_TOO_LONG').regex(/^[\u0600-\u06FFa-zA-Z\s]+$/, 'LAST_NAME_INVALID_CHARACTERS'),
     email: z.string({ error: (issue) => (issue.input === undefined ? 'EMAIL_REQUIRED' : undefined) }).trim().toLowerCase().email('EMAIL_INVALID_FORMAT'),
     password: z.string({ error: (issue) => (issue.input === undefined ? "PASSWORD_REQUIRED" : undefined) }).min(8, 'PASSWORD_TOO_SHORT').regex(/[A-Z]/, 'PASSWORD_MISSING_UPPERCASE').regex(/[a-z]/, 'PASSWORD_MISSING_LOWERCASE')
         .regex(/[0-9]/, 'PASSWORD_MISSING_NUMBER').regex(/[^A-Za-z0-9]/, 'PASSWORD_MISSING_SPECIAL_CHAR'),
@@ -35,7 +36,7 @@ export async function POST(request: NextRequest) {
         )
     }
 
-    const { name, email, password, phone } = parsed.data
+    const { firstName, lastName, email, password, phone } = parsed.data
     try {
         const existingUser = await prisma.user.findFirst({
             where: { OR: [{ email }, { phone }] },
@@ -57,10 +58,10 @@ export async function POST(request: NextRequest) {
         const passwordHash = await bcrypt.hash(password, 10)
         const newUser = await prisma.user.create({
             data: {
-                name, email, passwordHash, phone, role: 'customer'
+                firstName, lastName, email, passwordHash, phone, role: 'customer'
             },
             select: {
-                id: true, name: true, phone: true, email: true, role: true, createdAt: true
+                id: true, firstName: true, lastName: true, phone: true, email: true, role: true, createdAt: true
             }
         })
         return NextResponse.json(
